f9256b025a5178451cd3abd2b28b637166449f08
fanpeichao <fan552426811@outlook.com>
2022-11-15 17:28:18 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue';
+const handleChange = () => {};
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <div>
+      <fieldset>
+        <legend>请输入温度（单位：ccc）：</legend>
+        <input type="text" value="123" :onChange="handleChange" />
+      </fieldset>
+    </div>
+  </main>
+</template>
